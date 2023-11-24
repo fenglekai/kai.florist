1abc1f22b038cbb5bb6306514c5a432bca759754
fenglekai <674640130@qq.com>
2023-11-24 11:25:26 +0800

    feat(tabBar): 加载使用缓存数据

--- src/components/tabBar/index.tsx
+++ src/components/tabBar/index.tsx
@@ -1,17 +1,31 @@
 import { View } from "@tarojs/components";
-import Taro from "@tarojs/taro";
+import Taro, { useDidShow } from "@tarojs/taro";
 import { useState } from "react";
 import { AtTabBar } from "taro-ui";
 
 export default function TabBar() {
-  const [current, setCurrent] = useState(0)
+  useDidShow(() => {
+    try {
+      const index = Taro.getStorageSync('page-index')
+      if (!index) {
+        Taro.setStorageSync('page-index', 0)
+      } else {
+        setCurrent(index)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  });
 
+  const [current, setCurrent] = useState(0);
+
+  const dict = ["home", "cate", "user"];
   function handleClick(tab: number) {
-    setCurrent(tab)
-    const dict = ['home', 'cate', 'user'];
+    setCurrent(tab);
+    Taro.setStorageSync('page-index', tab)
     Taro.switchTab({
       url: `/pages/${dict[tab]}/index`,
-    })
+    });
   }
 
   return (
