fb641fe5fff6af278aa9ffed2f8a6c1ffe4ae147
fenglekai <674640130@qq.com>
2023-11-24 16:34:21 +0800

    feat(components): card使用定义类型

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,10 +1,8 @@
 import { View } from "@tarojs/components";
+import { useReactChild } from "types";
 import "./index.less";
 
-export default function Card(props: {
-  children: any;
-  className: string
-}) {
+export default function Card(props: { className: any; children: { title?: useReactChild; content: useReactChild; }; }) {
   return (<View className={`card ${props.className}`}>
     {
       props.children.title ? <View>{props.children.title}</View> : null
