c483fc0a0bea35f2ecd642cd5a4c499cdf9452b1
fenglekai <674640130@qq.com>
2023-12-08 17:27:53 +0800

    feat: 首页顶部布局变动

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import { View, Input, ScrollView, Text } from "@tarojs/components";
 import { AtIcon } from "taro-ui";
-import { useState } from "react";
-import { useLoad } from "@tarojs/taro";
+import { SetStateAction, useState } from "react";
+import { useLoad, navigateTo } from "@tarojs/taro";
 import WaterfallLayout from "@/components/waterfallLayout";
 import request from "@/utils/request";
+import { debounce } from "@/utils/common";
 import "./index.less";
 
 interface homeList {
@@ -21,9 +22,7 @@ export default function Home() {
   });
 
   const getHomeData = async () => {
-    const data: { data: homeList[] } = await request(
-      "/api/web/homeList"
-    );
+    const data: { data: homeList[] } = await request("/api/web/homeList");
     const formatData = data.data.map((item) => {
       return {
         ...item,
@@ -35,50 +34,87 @@ export default function Home() {
 
   const [list, setList] = useState<any[]>([]);
 
-  const [fixedSearch, setFixedSearch] = useState(false);
-
+  const [toTop, setToTop] = useState(true);
   const watchScroll = (e: any) => {
     const scrollTop = e.detail.scrollTop;
     if (scrollTop > 100) {
-      setFixedSearch(true);
+      setToTop(false);
     } else {
-      setFixedSearch(false);
+      setToTop(true);
     }
   };
 
-  return (
-    <View className="home">
-      {fixedSearch ? (
-        <View className="top-search fixed-search">
-          <View className="search-wrapper">
-            <View className="search-left">
-              <Input type="text" className="input" />
-            </View>
-            <View className="search-right">
-              <AtIcon value="search" size="30"></AtIcon>
-            </View>
+  const [searchInput, setSearchInput] = useState<string>();
+
+  function onInput(e: {
+    detail: { value: SetStateAction<string | undefined> };
+  }) {
+    setSearchInput(e.detail.value);
+  }
+
+  function handleSearch() {
+    navigateTo({
+      url: `/pages/search/index`,
+    });
+  }
+
+  function searchBuild() {
+    return (
+      <>
+        <View className="logo">
+          <Text
+            className="logo-text"
+            style={{
+              borderColor: toTop ? "#ffffff" : undefined,
+              color: toTop ? "#ffffff" : undefined,
+            }}
+          >
+            Florist
+          </Text>
+        </View>
+        <View
+          className="search-wrapper"
+          style={{
+            backgroundColor: toTop ? "#ffffff" : undefined,
+          }}
+          onClick={handleSearch}
+        >
+          <View className="search-content">
+            <Input
+              type="text"
+              className="input"
+              disabled
+              maxlength={20}
+              value={searchInput}
+              onInput={debounce(onInput, 1000)}
+            />
+          </View>
+          <View className="search-icon">
+            <AtIcon value="search" size="30"></AtIcon>
+          </View>
+          <View className="search-btn">
+            <Text>搜索</Text>
           </View>
         </View>
-      ) : null}
+      </>
+    );
+  }
 
+  return (
+    <View className="home">
       <ScrollView
         scrollY
         scrollWithAnimation
         onScroll={watchScroll}
         className="home-scroll"
       >
-        <View className="top-search">
-          <View className="logo">
-            <Text className="logo-text">Florist</Text>
-          </View>
-          <View className="search-wrapper">
-            <View className="search-left">
-              <Input type="text" className="input" />
-            </View>
-            <View className="search-right">
-              <AtIcon value="search" size="30"></AtIcon>
-            </View>
-          </View>
+        <View
+          className="top-search sticky-search"
+          style={{
+            backgroundColor: toTop ? "var(--fs-primary-color)" : undefined,
+          }}
+        >
+          {searchBuild()}
         </View>
         <WaterfallLayout className="home-waterfall" list={list}>
           {{ default: true }}
