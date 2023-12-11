75ae399ccce358877e5ffd5a518390fd4af23ff6
fenglekai <674640130@qq.com>
2023-12-11 17:19:15 +0800

    feat(component): 图片组件添加mode参数

--- src/components/imageCom/index.tsx
+++ src/components/imageCom/index.tsx
@@ -1,9 +1,11 @@
 import { Image } from "@tarojs/components";
+import type { ImageProps } from "@tarojs/components";
 import { useEffect, useState } from "react";
 import empty from "../../assets/image/empty.png";
 
 export default function ImageCom(props: {
   src: string;
+  mode?: keyof ImageProps.Mode;
   className?: string;
   onLoad?: () => void;
 }) {
@@ -25,6 +27,7 @@ export default function ImageCom(props: {
       onLoad={handleLoad}
       onError={handleError}
       fadeIn
+      mode={props.mode}
     ></Image>
   );
 }
