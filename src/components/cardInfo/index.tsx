132b88774ce92cc7d922c74330284853b91745ff
fenglekai <674640130@qq.com>
2023-12-11 17:18:08 +0800

    feat(component): 添加卡牌信息组件

---
+++ src/components/cardInfo/index.tsx
@@ -0,0 +1,36 @@
+import { View, Text } from "@tarojs/components";
+import { AtIcon } from "taro-ui";
+import ImageCom from "../imageCom";
+import "./index.less";
+
+export default function CardInfo(props: {
+  child: {
+    src: string;
+    title: string;
+    description: string;
+    price: number;
+    like_num: number;
+  };
+}) {
+  const { child } = props;
+  return (
+    <View className="card-wrapper">
+      <ImageCom src={child.src} mode="aspectFill" className="card-left" />
+      <View className="card-main">
+        <View>
+          <View className="card-primary-text text-over-one">{child.title}</View>
+          <View className="card-second-text text-over-two">
+            {child.description}
+          </View>
+        </View>
+        <View className="card-price-like">
+          <Text className="card-price card-primary-text">{child.price}</Text>
+          <View className="card-like">
+            <AtIcon value="heart"></AtIcon>
+            <Text>{child.like_num}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
