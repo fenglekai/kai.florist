381dc7b2eb3434c365994bb39930267d320b183b
fenglekai <674640130@qq.com>
2023-12-27 16:08:22 +0800

    feat(component): card样式变动

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,10 +2,10 @@ import { View } from "@tarojs/components";
 import { useReactChild } from "types";
 import "./index.less";
 
-export default function Card(props: { className: any; children: { title?: useReactChild; content: useReactChild; }; }) {
+export default function Card(props: { className?: any; children: { title?: useReactChild; content: useReactChild; }; }) {
   return (<View className={`card ${props.className}`}>
     {
-      props.children.title ? <View>{props.children.title}</View> : null
+      props.children.title ? <View className="title">{props.children.title}</View> : null
     }
     {
       props.children.content ? <View>{props.children.content}</View> : null
